388b2e430690ff38ce467e1eb358ff6594aa68e9
Vipin KUmar <126874481+vipinsatlok1@users.noreply.github.com>
2023-03-06 11:07:55 +0000

    save 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 9999
-const fs = require("fs")
+const fs = require('@cyclic.sh/s3fs') 
 
 
 let writer = fs.createWriteStream('hello.txt', {
