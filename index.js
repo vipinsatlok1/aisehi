a76310ed9194536d8823d2ac978c7ea7128f902a
Vipin KUmar <126874481+vipinsatlok1@users.noreply.github.com>
2023-03-06 14:23:51 +0000

    sasasa

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const app = express()
 const port = process.env.PORT || 9999
 const filevale = require('@cyclic.sh/s3fs')
 
-filevale.writeFileSync("satsaheb.txt", "hello ji", "utf-8")
+filevale.writeFile("satsahebji.txt", "hello ji mera naam vipin hai", "utf-8", (err, data) => {
+    console.log(err)
+    console.log(data)
+})
 
+console.log("data")
 
-console.log(filevale.readFileSync("satsaheb.txt"))
+filevale.readFile("satsahebji.txt", "utf-8", (err, data) => {
+    console.log(err)
+    console.log(data)
+})
+
 
 
 // let writer = filevale.createWriteStream('hello.txt', {
