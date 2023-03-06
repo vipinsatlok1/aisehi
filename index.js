3d0e256eea67e97cd7acc811edd824c65d0f8719
Vipin KUmar <126874481+vipinsatlok1@users.noreply.github.com>
2023-03-06 14:27:49 +0000

    bas aise hi dekho

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const app = express()
 const port = process.env.PORT || 9999
 const filevale = require('@cyclic.sh/s3fs')
 
-filevale.writeFile("satsahebji.txt", "hello ji mera naam vipin hai", "utf-8", (err, data) => {
-    filevale.readFile("satsahebji.txt", "utf-8", (err, data) => {
-        console.log(err)
-        console.log(data)
+
+
+app.get("/", (req, res) => {
+
+    filevale.writeFile("satsahebji.txt", "hello ji mera naam vipin hai", "utf-8", (err, data) => {
+        filevale.readFile("satsahebji.txt", "utf-8", (err, data) => {
+            res.send(data)
+        })
     })
+    
 })
 
 
